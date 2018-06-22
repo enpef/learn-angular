008c75119dcaf630da611711731888fea84b5a0f
unknown <natdanai.prs@gmail.com>
2018-06-22 17:29:46 +0700

    section 5 part 45

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { StocksComponent } from './stocks.component';
+import { Employee } from './employee';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Angular';
+  firstName = 'jon';
+  today = new Date();
+  employee : Employee = new Employee('Natdanai', 'Prasartkitjakorn', 50000);
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MutualfundsComponent } from './mutualfunds/mutualfunds.component';
+import { StocksComponent } from './stocks.component';
+import { HighLightDirective} from './highlight.directive';
+import { StockService } from './stock.service';
+import { DateFormatterPipe } from './date-formatter.pipe';
+import { FormsModule } from '@angular/forms';
+import { CapitalizePipe } from './capitalize.pipe';
+import { EmployeenamePipe } from './employeename.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MutualfundsComponent,
+    StocksComponent,
+    HighLightDirective,
+    DateFormatterPipe,
+    CapitalizePipe,
+    EmployeenamePipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [StockService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
